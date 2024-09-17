23f8ce9e8f0c1563b0ac890c7284d6b5542ed353
Oluwadamilola Adebayo <o.adebayo2@univerisityofgalway.ie>
2024-09-17 17:36:18 +0100

    test

--- Card.java
+++ Card.java
@@ -9,8 +9,31 @@ public class Card {
         this.number = number;
     }
 
-    public String getCard(){
+
+    public String getCard(String color, String shape, int number){
         String card = "##############";
+        int cardLength = 12;
+        int cardWidth = 12;
+        Object[] cardDetail = new Object[3];
+        cardDetail[0] = new String(color);
+        cardDetail[2] = new Integer(number);
+        cardDetail[1] = new String(shape);
+        int cardDetailIndex = 0;
+        for (int i = 0; i < cardLength; i++){
+            if(i % 3 == 0){
+                String line = "";
+                int length = cardDetail[cardDetailIndex].toString().length();
+                int before = 0;
+                int after = 0;
+                for (int j = 0; j < cardWidth; j++){
+                    line 
+                }
+
+                cardDetailIndex++;
+
+            }
+
+        }
         card +=       "#            #";
         card +=       "#            #";
         card +=       "#            #";
